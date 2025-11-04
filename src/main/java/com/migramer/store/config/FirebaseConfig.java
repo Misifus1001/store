c9d60d08e638be146bacaf573c7d690b355e1d0c
RobertoMaxikash <roberto.cruz@maxikash.mx>
2025-11-04 09:58:11 -0600

    Se implementan push notification

---
+++ src/main/java/com/migramer/store/config/FirebaseConfig.java
@@ -0,0 +1,35 @@
+package com.migramer.store.config;
+
+import java.io.InputStream;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initFirebase() throws Exception {
+        InputStream serviceAccount = 
+                getClass().getClassLoader().getResourceAsStream("firebase/firebase-admin.json");
+
+        if (serviceAccount == null) {
+            throw new IllegalStateException("No se encontró firebase-admin.json");
+        }
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+            System.out.println("Firebase Admin inicializado");
+        }
+    }
+    
+}
--- src/main/java/com/migramer/store/controllers/TestController.java
+++ src/main/java/com/migramer/store/controllers/TestController.java
@@ -1,6 +1,10 @@
 package com.migramer.store.controllers;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +12,14 @@ public class TestController {
 
     @GetMapping("/public/saludar")
     public String saludar() {
+        return "Hola :)";
+    }
+
+    @PostMapping("/api/push-notification")
+    public String pushNotification(@RequestBody Map<String, Object> payload) {
+
+        System.out.println(payload);
+        
         return "Hola :)";
     }
 
