f0b2b7e212e6f10ea8496d35b2b7cf1938d9b4fd
RobertoMaxikash <roberto.cruz@maxikash.mx>
2025-12-06 00:29:21 -0600

    Se configura el envío de push notifications cada 10 minutos

--- src/main/java/com/migramer/store/service/SchedulerService.java
+++ src/main/java/com/migramer/store/service/SchedulerService.java
@@ -14,7 +14,7 @@ public class SchedulerService {
 
     // @Scheduled(cron = "0 45 8 1/15 * ?")
     // @Scheduled(cron = "0 */10 * * * *")
-    @Scheduled(cron = " 0 1/1 * * * *")
+    @Scheduled(cron = " 0 */10 * * * *")
     public void pushNotification() {
         pushNotificationService.enviarPushNotifications();
     }
